5fc3e46e8220421589ebdd6825e10839a6462180
Steve Balayan <steve.balayan@gmail.com>
2020-04-16 18:36:53 +0300

    Backend and frontend scripts done
    Done normal backend and frontend. Still there are some things to do, but the MVP is done.

--- backend.js
+++ backend.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const app = express();
 
+let carAccidents = {};
+carAccidents['2016'] = require('./2016.json');
+carAccidents['2017'] = require('./2017.json');
+carAccidents['2018'] = require('./2018.json');
+
 app.use(express.static('public')); //все данные лежат в папке public
 app.get('/hello', function(req, res) {
-    res.send('Hello!')
+    res.send('Server is on!');
 });
 
-app.listen(3000, function() {
-    console.log('Successfully run on port 3000!');
+//отправляем с сервера описания ДТП в регионе запроса
+app.get('/car_accident_in_region', function(req, res) {
+    let regionName = req.query.regionName;
+    let year = req.query.year;
+    //тестовый вариант, идём не по всему файлу
+    //let size = carAccidents[year].length;
+    let size = 100;
+    let arr = [];
+    for (let i = 0; i < size; i++) {
+        if (carAccidents[year][i]['reg_name'] === regionName) {
+            arr.push([ Number(carAccidents[year][i]['latitude']), Number(carAccidents[year][i]['longitude']) ]);
+        }
+    }
+    res.send(arr);
+});
+
+//поднимаем на локалхосте
+app.listen(8080, function() {
+    console.log('Successfully run on port 8080!');
 });
--- public/frontend.js
+++ public/frontend.js
@@ -1,8 +1,98 @@
-ymaps.ready(init);
+let myMap;
+let objectManager;
 
-function init(){
-    let myMap = new ymaps.Map("map", {
-        center: [55.76, 37.64],
-        zoom: 7
+document.addEventListener('DOMContentLoaded', init);
+
+//инициализация всего
+//TODO -- 100500 кнопок переписать на нормальные менюшки
+function init() {
+    //проверка работы сервера
+    let testServer = document.getElementById('testServer');
+    testServer.addEventListener('click', () => {
+        axios.get('/hello').then(function (response) {
+            console.log(response);
+            console.log('Server is on!');
+        });
+    });
+    ymaps.ready(initMap);
+    //поисковая строка отображения ДТП по регионам
+    let searchByRegion = document.getElementById('searchByRegion');
+    //дополнительное поле под год -- TODO: сделать выползающей менюшкой
+    let searchByYear = document.getElementById('searchByYear');
+    //кнопка отправки запроса поисковой строки выше
+    let btnSendSearchByRegion = document.getElementById('btnSendSearchByRegion');
+    //кнопка очистки карты, полной очистки
+    let clearMap = document.getElementById('clearMap');
+    clearMap.addEventListener('click', () => {
+        myMap.geoObjects.removeAll();
+    })
+    
+    //тут для красоты
+    searchByRegion.value= "Название региона";
+    searchByYear.value= "Год поиска";
+    searchByRegion.addEventListener('click', () => {
+        searchByRegion.value = "";
+    });
+    searchByYear.addEventListener('click', () => {
+        searchByYear.value = "";
+    });
+
+    //запрашиваем у сервера данные для отображения
+    btnSendSearchByRegion.addEventListener('click', querySearchByRegion);
+}
+
+//инициализация самой карты
+function initMap() {
+    myMap = new ymaps.Map(
+        'map',
+        {
+            center: [55.76, 37.64],
+            zoom: 5
+        },
+        {
+            searchControlProvider: 'yandex#search'
+        }
+    );
+    objectManager = new ymaps.ObjectManager({
+        // Чтобы метки начали кластеризоваться, выставляем опцию.
+        clusterize: true,
+        // ObjectManager принимает те же опции, что и кластеризатор.
+        gridSize: 32,
+        clusterDisableClickZoom: true
+    });
+    myMap.geoObjects.add(objectManager);
+}
+
+//запрос на сервер по региону и году
+function querySearchByRegion() {
+    let region = searchByRegion.value;
+    let year = searchByYear.value;
+    searchByRegion.value = "";
+    searchByYear.value = "";
+    axios.get('/car_accident_in_region', { params: { regionName: region, year: year } }).then(function (response) {
+        console.log(response);
+        console.log(response.data.length);
+        showAccidents(response.data, year);
     });
 }
+
+//отображение меток, TODO -- задавать цвет в зависимости от года
+function showAccidents(carAccidents, year) {
+    console.log(carAccidents);
+    objectManager.objects.options.set('preset', 'islands#greenDotIcon');
+    objectManager.clusters.options.set('preset', 'islands#greenClusterIcons');
+    let result = [];
+    for (let i = 0; i < carAccidents.length; i++) {
+        let item =  {
+            type: 'Feature',
+            id: i,
+            geometry: {
+                type: 'Point',
+                coordinates: carAccidents[i]
+            } 
+        }
+        result.push(item);
+    }
+    objectManager.add(result);
+}
+
